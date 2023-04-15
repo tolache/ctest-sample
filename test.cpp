b43e71caec27bcdf94dc3696621c0a8c6cf23252
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2023-04-15 10:35:08 +0200

    revert test.cpp

--- test.cpp
+++ test.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 #include <cassert>
 
-int main(int argc, char** argv) {
-    std::cout << "Running test " << argv[1] << "..." << std::endl;
+int main() {
+    std::cout << "Running tests..." << std::endl;
 
-    if (argv[1][0] == '1') {
-        assert(true);
-        std::cout << "Test 1 passed." << std::endl;
-    }
-    else if (argv[1][0] == '2') {
-        assert(false);
-        std::cout << "Test 2 failed." << std::endl;
-    }
+    assert(true);
 
+    std::cout << "All tests passed." << std::endl;
     return 0;
 }
