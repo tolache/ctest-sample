d7015dc2ba4ab22f8f8b47baeef1f5a1893e7b95
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2023-04-15 09:10:29 +0200

    remove build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-mkdir $SCRIPT_DIR/build
-cd $SCRIPT_DIR/build
-cmake ..
-cmake --build .
-ctest -VV
